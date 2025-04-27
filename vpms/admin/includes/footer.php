e6f200278f6fa4b0f0b634d16f0307c472384bdd
Muhammad Ezral Redzuan <125541594+ezralredzuan@users.noreply.github.com>
2025-04-27 14:54:34 +0800

    this is
    this is

---
+++ vpms/admin/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    
+                    <div class="col-sm-6 text-center">
+                    E-Vehicle Sticker Application for staff
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ vpms/users/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                   
+                    <div class="col-sm-12 text-center">
+                        E-Vehicle Sticker Application System
+                    </div>
+                </div>
+            </div>
+        </footer>
